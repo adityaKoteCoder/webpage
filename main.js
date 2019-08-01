7c43e4b02333604ab234a65a233141ccd59309a4
dsouzajoy <akstr17official@gmail.com>
2019-08-01 16:31:30 +0530

    update

---
+++ main.js
@@ -0,0 +1,20 @@
+function factorial()
+{
+            num = parseInt(document.getElementById("num").value)
+            res = 1
+            for(let i)
+}
+
+function digitsum()
+{
+
+}
+function palindrome()
+{
+
+}
+
+function reverse()
+{
+
+}
